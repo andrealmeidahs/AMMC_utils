61b725a7cb33b31063c0a4ed8fde3b182374b099
Andre Almeida <andre.almeida@hypercubescientific.com.au>
2025-03-16 19:43:15 +1100

    working jaamsim parser

--- jaamsim_parser.py
+++ jaamsim_parser.py
@@ -1,4 +1,5 @@
 import re
+import sys
 import argparse
 
 def parse_args():
@@ -6,63 +7,87 @@ def parse_args():
     parser.add_argument("filename", help="Jaamsim file to parse")
     return parser.parse_args()
 
-def parse_statement(s):
-    n_brackets = 0
-    in_quote = False
+def parse_line(s, n_brackets=0, in_quote=False, in_comment=False):
 
     els = []
+    parent_el = els
     cur_el = ""
     for c in s:
-        if c == '{' and not in_quote:
-            n_brackets += 1
-        elif c == '}' and not in_quote:
-            n_brackets -= 1
-        elif c == "'":
-            in_quote = not in_quote
-        elif c == ' ' or c == '\t':
-            els += cur_el
-            cur_el = ""
-        else:
-            cur_el += c 
-    return els
-
-def parse_line(line, cur_statement=""):
-    cur_statement += line.strip()
-    if re.match(r"^include", cur_statement):
-        type = "inc"
-        els = re.match(r"^include\s+(.*)$", cur_statement).groups()[0]
-        return type, els
-    elif re.match(l,"^Define"):
-        type = "def"
-        els = re.match(r"^Define\s+(.*)$", cur_statement).groups()[0]
-        return type, els
-    elif re.match(l,"^RecordEdits"):
-        type = "st"
-        return 
-    else:
-        type = ""
-        els = cur_statement
-    
+        if c == "#" and not in_quote:
+            in_comment = True
+        if not in_comment:
+            if (c == '{') and not in_quote:
+                n_brackets += 1
+                parent_el.append([])
+                grandparent_el = parent_el
+                parent_el = parent_el[-1]
+            elif (c == '}') and not in_quote:
+                #sys.stderr.write(f"Reducing n brackets to {n_brackets}\n")
+                n_brackets -= 1
+                # sys.stderr.write(f"el: {cur_el}\nparent: {parent_el}\ngparent: {grandparent_el}\n")
+                if len(cur_el)>0:
+                    parent_el.append(cur_el)
+                # el = parent_el.copy()
+                parent_el = els
+                for nb in range(n_brackets):
+                    parent_el = parent_el[-1]
+                # parent_el.append(el)
+                cur_el = ""
+            elif c == "'":
+                in_quote = not in_quote
+            elif (c == ' ' or c == '\t' or c == '\n') and not in_quote:
+                if len(cur_el)>0:
+                    parent_el.append(cur_el)
+                cur_el = ""
+            elif not in_comment:
+                cur_el += c
+            
+            
+    return els, n_brackets, in_quote, in_comment
 
 def parse_jaamsim(filename, includes=False):
+    buffer = ""
+    el_list = []
+
+    n_brackets = 0
+    in_comment = False
+    in_quote = False
     with open(filename, "r") as f:
         for line in f:
-            parts, n_open_brackets = parse_line(line)
-            if n_open_brackets == 0:
-                els = parse_parts(parts)
-            if line[:6] == "include":
+            buffer += line
+            (els, n_brackets,
+             in_quote ,
+             in_comment) = parse_line(buffer,
+                                      n_brackets=0,
+                                      in_quote=False,
+                                      in_comment=in_comment)
+            in_comment = False
+            if n_brackets < 1:
+                if len(els) > 0:
+                    sys.stderr.write(f"> {buffer}\n")
+                    sys.stderr.write(f"{els}\n")
+                    el_list.append(els)
+                buffer = ""
+                if n_brackets < 0:
+                    sys.stderr.write("Unmatched brackets")
+                n_brackets = 0
+            # else:
+                # sys.stderr.write(f"{n_brackets},{in_quote}\n")
+            #     sys.stderr.write("Unfinished line")
+            #     sys.stderr.write(buffer)
+
+            if line[:6] == "include" and includes:
                 # write logic to include file
                 continue
-            
-
-
-
+    return el_list
 
 
 if __name__ == "__main__":
     args = parse_args()
     filename = args.filename
-    parse_jaamsim(filename)
+    els = parse_jaamsim(filename)
+    for el in els:
+        print(el)
 
 
     
