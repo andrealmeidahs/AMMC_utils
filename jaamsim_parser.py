7e5fb3118d67590ce7a963ef1e3eca8fac1a4f3f
andrealmeidahs <andre.almeida@hypercubescientific.com.au>
2025-04-07 17:27:56 +1000

    WIP object parser

--- jaamsim_parser.py
+++ jaamsim_parser.py
@@ -17,103 +17,107 @@ def parse_args():
     parser.add_argument("filename", help="Jaamsim file to parse")
     return parser.parse_args()
 
-def parse_line(s, prev_parts=[], n_brackets=0, in_quote=False, in_comment=False):
+class JaamsimParser(object):
+    def __init__(self, filename):
+        self.struct = {}
+        self.open_brackets = 0
+        self.open_quote = False
+        self.comment = ""
+        self.open_comment = False
+        self.current_file = filename
+        self.current_line = 0
+        self.current_col = 0
+        self.el_col = 0
+        self.current_els = []
 
-    els = prev_parts
-    parent_el = els
-    for nb in range(n_brackets):
-        parent_el = parent_el[-1]
-    cur_el = ""
-    comment = None
-    for chno, c in enumerate(s):
-        if c == "#" and not in_quote:
-            in_comment = True
-            comment = s[chno+1:]
-            break
-        if not in_comment:
-            if (c == '{') and not in_quote:
-                n_brackets += 1
-                parent_el.append([])
-                parent_el = parent_el[-1]
-            elif (c == '}') and not in_quote:
-                #sys.stderr.write(f"Reducing n brackets to {n_brackets}\n")
-                n_brackets -= 1
-                # sys.stderr.write(f"el: {cur_el}\nparent: {parent_el}\ngparent: {grandparent_el}\n")
-                if len(cur_el)>0:
-                    el_compl = {"contents":cur_el.strip(),
-                                "comment": comment,
-                                "file": "",}
-                    parent_el.append(cur_el.strip())
-                # el = parent_el.copy()
-                parent_el = els
-                for nb in range(n_brackets):
+    def wrap_element(self, el):
+        return {"contents": el.strip(),
+                    "comment": self.comment,
+                    "line": self.current_line,
+                    "column": self.el_col,
+                    "file": self.current_file}
+
+    def parse_line(self, s):
+        parent_el = self.current_els
+        for nb in range(self.open_brackets):
+            parent_el = parent_el[-1]
+        cur_el = ""
+        for chno, c in enumerate(s):
+            if c == "#" and not self.open_quote:
+                self.open_comment = not self.open_comment
+                self.comment = s[chno+1:]
+                break
+            if not self.open_comment:
+                if (c == '{') and not self.open_quote:
+                    self.open_brackets += 1
+                    parent_el.append([])
                     parent_el = parent_el[-1]
-                # parent_el.append(el)
-                cur_el = ""
-            elif c == "'":
-                in_quote = not in_quote
-            elif (c == ' ' or c == '\t' or c == '\n') and not in_quote:
-                if len(cur_el)>0:
-                    parent_el.append(cur_el.strip())
-                cur_el = ""
-            else:
-                cur_el += c
-            
-    return els, n_brackets, in_quote, in_comment
-
-def parse_jaamsim(filename, includes=False):
-
-    base_path, root_file = os.path.split(filename)
-    
-    buffer = ""
-    el_list = []
-    el_dict = {}
-
-    n_brackets = 0
-    in_comment = False
-    in_quote = False
-    els = []
-    with open(filename, "r") as f:
-        for line in f:
-            sys.stderr.write(line+'\n')
-            parts = line.strip().split()
-            if len(parts)>0 and parts[0].lower() == "include":
-                incfile = os.path.join(base_path, parts[1].strip("'"))
-                el_inc = parse_jaamsim(incfile, includes=True)
-                deep_update(el_dict, el_inc)
-                el_list.extend(el_inc)
-                continue
-            (els, n_brackets,
-             in_quote ,
-             in_comment) = parse_line(line,
-                                      prev_parts=els,
-                                      n_brackets=n_brackets,
-                                      in_quote=in_quote,
-                                      in_comment=in_comment)
-            in_comment = False
-            if n_brackets < 1:
-                if len(els) > 0:
-                    sys.stderr.write(f"> {buffer}\n")
-                    el_list.append(els)
-                    obj = els[0]
-                    try:
-                        el_dict[obj]
-                    except KeyError:
-                        el_dict[obj] = {}
-                    for ii in range(1,len(els),2):
-                        el_dict[obj][els[ii]] = els[ii+1]
-                    #sys.stderr.write(f"{el_dict[obj]}\n")
+                elif (c == '}') and not self.open_quote:
+                    #sys.stderr.write(f"Reducing n brackets to {n_brackets}\n")
+                    self. open_brackets -= 1
+                    # sys.stderr.write(f"el: {cur_el}\nparent: {parent_el}\ngparent: {grandparent_el}\n")
+                    if len(cur_el)>0:
+                        parent_el.append(self.wrap_element(cur_el))
+                    # el = parent_el.copy()
+                    parent_el = self.current_els
+                    for nb in range(self.open_brackets):
+                        parent_el = parent_el[-1]
                     
-                if n_brackets < 0:
-                    sys.stderr.write("Unmatched brackets")
-                n_brackets = 0
-                els = []
-            # else:
-                # sys.stderr.write(f"{n_brackets},{in_quote}\n")
-            #     sys.stderr.write("Unfinished line")
-            #     sys.stderr.write(buffer)
+                    # parent_el.append(el)
+                    cur_el = ""
+                elif c == "'":
+                    self.open_quote = not self.open_quote
+                elif (c == ' ' or c == '\t' or c == '\n') and not self.open_quote:
+                    if len(cur_el)>0:
+                        parent_el.append(self.wrap_element(cur_el))
+                    cur_el = ""
+                else:
+                    cur_el += c
+            
+        if self.open_brackets < 1:
+            if len(self.current_els) > 0:
+                obj = self.current_els[0]
+                try:
+                    self.struct[obj]
+                except KeyError:
+                    self.struct[obj] = {}
+                for ii in range(1,len(self.current_els),2):
+                    self.struct[obj][self.current_els[ii]] = self.current_els[ii+1]
+            self.current_els = []
+                    
+        self.open_comment = False
+        self.comment = ""
+        self.current_line += 1
+        self.current_col = 0
 
-    return el_dict
+    def parse_file(self,  includes=False):
+        base_path, root_file = os.path.split(filename)
+    
+        with open(filename, "r") as f:
+            for line in f:
+                sys.stderr.write(line+'\n')
+                parts = line.strip().split()
+                if len(parts)>0 and parts[0].lower() == "include":
+                    incfile = os.path.join(base_path, parts[1].strip("'"))
+                    el_inc = parse_jaamsim(incfile, includes=True)
+                    deep_update(self.struct, el_inc)
+                    continue
+                self.parse_line(line)
+                        #sys.stderr.write(f"{el_dict[obj]}\n")
+                    
+                if self.open_brackets < 0:
+                    sys.stderr.write("Unmatched brackets")
+                # else:
+                    # sys.stderr.write(f"{n_brackets},{in_quote}\n")
+                #     sys.stderr.write("Unfinished line")
+                #     sys.stderr.write(buffer)
+
+        return self.struct
+
+def parse_jaamsim(filename, includes=True):
+    parser = JaamsimParser("filename")
+    parser.parse_file(includes=True)
+    return parser.sturct
 
 
 if __name__ == "__main__":
